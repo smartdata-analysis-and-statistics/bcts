8dbe4d07c31eed62f0773a133b7879543e37ca09
Thomas Debray <117118104+tdebray123@users.noreply.github.com>
2024-07-11 10:28:33 +0200

    update code

--- CIPO/run_sim.r
+++ CIPO/run_sim.r
@@ -1,5 +1,5 @@
 # Evaluate type-I error over a grid of gamma
-nsim <- 10000
+nsim <- 50000
 gammas <- seq(from = 0.98, to = 0.99, by = 0.001)
 
 results <- data.frame(gamma = numeric(),
@@ -9,46 +9,79 @@ results <- data.frame(gamma = numeric(),
                       ciu = numeric())
 
 for (i in seq(gammas)) {
-  sim <- eval_bcts(n_per_arm_int = 20,
-                   n_per_arm_pln = 65,
-                   n_per_arm_max = 80,
-                   block.sizes = 1,
-                   mu_c = 0, sd_c = 1,
-                   mu_t = c("Velusetrag 15mg" = 0.4, "Velusetrag 30mg" = 0.5), #
-                   sd_t = c("Velusetrag 15mg" = 1, "Velusetrag 30mg" = 1), #
-                   prioritize_low_trteff = TRUE,
-                   gamma = gammas[i], #   %
-                   th.fut = 0.2, th.eff = 0.9, th.prom = 0.5, ##
-                   nsim = nsim,
-                   num_chains = 4,
-                   n.iter = 10, #
-                   n.adapt = 500,
-                   perc_burnin = 0.2,
-                   progress.bar = "text")
+  sim <- bcts(n_int = 20*3,
+              n_pln = 20 + 65*2,
+              n_max = 20 + 80*2,
+              mu = c("Placebo" = 0, "Velusetrag 15mg" = 0.4, "Velusetrag 30mg" = 0.5),
+              sigma = c("Placebo" = 1, "Velusetrag 15mg" = 1, "Velusetrag 30mg" = 1),
+              trt_ref = "Placebo",
+              prioritize_low_rank = TRUE, gamma = gammas[i], #   %
+              method = "mcmc",
+              th.fut = 0.2, th.eff = 0.9, th.prom = 0.5, ##
+              nsim = 10000)
   results <- results %>% add_row(cbind(gamma = gammas[i], sim))
 }
 
-saveRDS(results, file = "CIPO/sim20.65.80.rds")
+saveRDS(results, file = "CIPO/sim60.150.180.rds")
+
+results2 <- data.frame(gamma = numeric(),
+                      statistic = character(),
+                      est = numeric(),
+                      cil = numeric(),
+                      ciu = numeric())
+
+for (i in seq(gammas)) {
+  sim <- bcts(n_int = 20*3,
+              n_pln = 20 + 70*2,
+              n_max = 20 + 80*2,
+              mu = c("Placebo" = 0, "Velusetrag 15mg" = 0.4, "Velusetrag 30mg" = 0.5), #
+              sigma = c("Placebo" = 1, "Velusetrag 15mg" = 1, "Velusetrag 30mg" = 1), #
+              trt_ref = "Placebo",
+              prioritize_low_rank = TRUE, gamma = gammas[i], #   %
+              method = "mcmc",
+              th.fut = 0.2, th.eff = 0.9, th.prom = 0.5, ##
+              nsim = 10000)
+  results2 <- results2 %>% add_row(cbind(gamma = gammas[i], sim))
+}
+
+saveRDS(results2, file = "CIPO/sim20.70.80.rds")
 
 
-# Plot Power and type-I error
-ggplot(results %>% filter(statistic %in% c("Power", "Type-I error")), aes(x=gamma, y = est, ymin = cil, ymax = ciu, color = statistic)) +
-  geom_point() +
-  xlab("Gamma") +
-  geom_errorbar() +
-  geom_hline(yintercept = 0.03, lty = 2, col = "red") +
-  geom_hline(yintercept = 0.025, lty = 2, col = "blue") +
-  geom_hline(yintercept = 0.8, lty = 2, col = "grey") +
-  ggtitle("N = 20; 65; 80")
 
-# Plot Pr(inc.ss) and Pr(fut.trig)
-ggplot(results %>% filter(statistic %in% c("Pr(fut.trig)", "Pr(inc.ss)", "Pr(sel.high)")),
-       aes(x=gamma, y = est, ymin = cil, ymax = ciu)) +
-  geom_point() +
-  xlab("Gamma") +
-  geom_errorbar() +
-  ggtitle("N = 20; 65; 80") + facet_wrap(~statistic)
+sim98 <- bcts(n_int = 20*3,
+                   n_pln = 20 + 65*2,
+                   n_max = 20 + 80*2,
+                   mu = c("Placebo" = 0, "Velusetrag 15mg" = 0.4, "Velusetrag 30mg" = 0.5), #
+                   sigma = c("Placebo" = 1, "Velusetrag 15mg" = 1, "Velusetrag 30mg" = 1), #
+                   trt_ref = "Placebo",
+                  prioritize_low_rank = TRUE, gamma = 0.98, #   %
+                  method = "mcmc",
+                  th.fut = 0.2, th.eff = 0.9, th.prom = 0.5, ##
+                  nsim = 10000,
+                  num_chains = 4,
+                  n.iter = 10, #
+                  n.adapt = 500,
+                  perc_burnin = 0.2,
+                  progress.bar = "text")
+saveRDS(sim98, file = "CIPO/gamma98N20.65.80.rds")
 
+
+sim98b <- bcts(n_int = 20*3,
+              n_pln = 20 + 65*2,
+              n_max = 20 + 80*2,
+              mu = c("Placebo" = 0, "Velusetrag 15mg" = 0.4, "Velusetrag 30mg" = 0.5), #
+              sigma = c("Placebo" = 1, "Velusetrag 15mg" = 1, "Velusetrag 30mg" = 1), #
+              trt_ref = "Placebo",
+              prioritize_low_rank = TRUE, gamma = 0.98, #   %
+              method = "bayes",
+              th.fut = 0.2, th.eff = 0.9, th.prom = 0.5, ##
+              nsim = 10000,
+              num_chains = 4,
+              n.iter = 10, #
+              n.adapt = 500,
+              perc_burnin = 0.2,
+              progress.bar = "text")
+saveRDS(sim98, file = "CIPO/gamma98N20.65.80.rds")
 
 
 
